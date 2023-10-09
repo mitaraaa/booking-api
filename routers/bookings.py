2e8fa701f047d80429aa2784b3af600133638cf2
mitaraaa <rustam030201@gmail.com>
2023-10-09 15:00:33 +0600

    added missing routes

--- routers/bookings.py
+++ routers/bookings.py
@@ -1,35 +1,200 @@
-from dataclasses import dataclass
-from datetime import datetime
-from typing import Optional
-from fastapi import APIRouter, status, HTTPException
-from db import session, Booking
+from datetime import date, datetime
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from pydantic import BaseModel
 from sqlalchemy.exc import IntegrityError
+from sqlmodel import select
 
+from db import Booking, FootballField, User, session
 from db.models.booking import BookingStatus
-
+from routers.auth import get_admin_user, get_authenticated_user
 
 router = APIRouter(prefix="/bookings")
 
 
-@dataclass
-class BookingData:
-    user_id: int
-    field_id: int
+class BookingData(BaseModel):
+    user_id: int | None
+    field_id: int | None
 
-    booking_date: datetime
-    booked_until: datetime
+    booking_date: datetime | None
+    booked_until: datetime | None
 
-    status: Optional[BookingStatus] = None
+    status: BookingStatus | None = BookingStatus.pending
+
+
+class BookingTime(BaseModel):
+    booking_date: datetime | None
+    booked_until: datetime | None
+
+    def __str__(self) -> str:
+        return f"{self.booking_date} - {self.booked_until}"
+
+
+class TargetDate(BaseModel):
+    target_date: date
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def create_booking(data: BookingData):
+def create_booking(
+    data: BookingData, user: User = Depends(get_authenticated_user)
+):
     with session:
         try:
             booking = Booking(**(dict(vars(data).items())))
+            booking.user_id = user.id
+
             session.add(booking)
             session.commit()
+
+            session.refresh(booking)
+            return booking.json()
         except IntegrityError:
             raise HTTPException(
                 status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
             )
+
+
+@router.get(
+    "/", status_code=status.HTTP_200_OK, dependencies=[Depends(get_admin_user)]
+)
+def get_bookings():
+    with session:
+        stmt = select(Booking)
+        return [booking.json() for booking in session.scalars(stmt)]
+
+
+@router.get("/user", status_code=status.HTTP_200_OK)
+def get_user_bookings(user: User = Depends(get_authenticated_user)):
+    with session:
+        stmt = select(Booking).where(Booking.user_id == user.id)
+        return [booking.json() for booking in session.scalars(stmt)]
+
+
+@router.get("/field/{field_id}", status_code=status.HTTP_200_OK)
+def get_field_bookings(field_id: int):
+    with session:
+        stmt = select(Booking).where(Booking.field_id == field_id)
+
+        bookings = []
+        for booking in session.scalars(stmt):
+            booking: dict = booking.json()
+            booking.pop("user_id")
+            bookings.append(booking)
+
+        return bookings
+
+
+@router.get("field/{field_id}/availability", status_code=status.HTTP_200_OK)
+def get_field_availability(field_id: int, target_date: TargetDate):
+    with session:
+        stmt = select(FootballField).where(FootballField.id == field_id)
+        field: FootballField = session.scalar(stmt)
+
+        if not field:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Field not found",
+            )
+
+        stmt = select(Booking).where(Booking.field_id == field_id)
+
+        bookings = []
+        for index, booking in enumerate(session.scalars(stmt)):
+            booking: dict = booking.json()
+            if booking["booking_date"].date() != target_date.target_date:
+                continue
+
+            if index == 0:
+                bookings.append(
+                    BookingTime(
+                        datetime.combine(
+                            target_date.target_date, field.start_time
+                        ),
+                        booking["booking_date"],
+                    )
+                )
+                continue
+
+            bookings.append(
+                BookingTime(bookings[-1].booked_until, booking["booking_date"])
+            )
+
+        return bookings
+
+
+@router.get("/{booking_id}", status_code=status.HTTP_200_OK)
+def get_booking(booking_id: int):
+    with session:
+        stmt = select(Booking).where(Booking.id == booking_id)
+        booking: Booking = session.scalar(stmt)
+
+        if not booking:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Booking not found",
+            )
+
+        return booking.json()
+
+
+@router.put("/{booking_id}", status_code=status.HTTP_200_OK)
+def update_booking(
+    booking_id: int,
+    data: BookingData,
+    user: User = Depends(get_authenticated_user),
+):
+    with session:
+        stmt = select(Booking).where(Booking.id == booking_id)
+        booking: Booking = session.scalar(stmt)
+
+        if not booking:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Booking not found",
+            )
+
+        if booking.user_id != user.id:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not allowed to update this booking",
+            )
+
+        booking.booking_date = data.booking_date
+        booking.booked_until = data.booked_until
+        booking.status = data.status
+
+        session.add(booking)
+        session.commit()
+
+        session.refresh(booking)
+        return {
+            "message": "Booking updated successfully",
+            "booking": booking.json(),
+        }
+
+
+@router.delete("/{booking_id}", status_code=status.HTTP_200_OK)
+def delete_booking(
+    booking_id: int,
+    user: User = Depends(get_authenticated_user),
+):
+    with session:
+        stmt = select(Booking).where(Booking.id == booking_id)
+        booking: Booking = session.scalar(stmt)
+
+        if not booking:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Booking not found",
+            )
+
+        if booking.user_id != user.id:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not allowed to update this booking",
+            )
+
+        session.delete(booking)
+        session.commit()
+
+        return {"message": "Booking deleted successfully"}
--- routers/fields.py
+++ routers/fields.py
@@ -1,37 +1,132 @@
-from dataclasses import dataclass
-from datetime import datetime
-from typing import Optional
-from fastapi import APIRouter, status, HTTPException
-from db import session, FootballField
-from sqlalchemy.exc import IntegrityError
+from datetime import time
 
+from fastapi import APIRouter, Depends, HTTPException, status
+from pydantic import BaseModel
+from sqlalchemy.exc import IntegrityError
+from sqlmodel import select
+
+from db import FootballField, Owner, session
+from routers.auth import get_authenticated_owner
 
 router = APIRouter(prefix="/fields")
 
 
-@dataclass
-class FieldData:
-    owner_id: int
+class FieldData(BaseModel):
+    owner_id: int | None
 
-    name: str
-    location: str
-    surface_type: Optional[str]
+    name: str | None
+    location: str | None
+    surface_type: str | None
 
-    width: float
-    height: float
+    width: float | None
+    height: float | None
 
-    start_time: datetime
-    end_time: datetime
+    start_time: time | None
+    end_time: time | None
+
+    def __init__(self, **data):
+        for key, value in data.items():
+            if key in ["start_time", "end_time"] and isinstance(value, str):
+                data[key] = time.fromisoformat(value)
+
+        super().__init__(**data)
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def create_field(data: FieldData):
+def create_field(
+    data: FieldData, owner: Owner = Depends(get_authenticated_owner)
+):
     with session:
         try:
             field = FootballField(**(dict(vars(data).items())))
+            field.owner_id = owner.id
+
             session.add(field)
             session.commit()
+
+            return {"message": "Field created successfully"}
         except IntegrityError:
             raise HTTPException(
                 status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
             )
+
+
+@router.get("/owner", status_code=status.HTTP_200_OK)
+def get_owner_fields(owner: Owner = Depends(get_authenticated_owner)):
+    with session:
+        stmt = select(FootballField).where(FootballField.owner_id == owner.id)
+        return [x.json() for x in session.scalars(stmt)]
+
+
+@router.put(
+    "/{field_id}",
+    status_code=status.HTTP_200_OK,
+)
+def update_field(
+    field_id: int,
+    data: FieldData,
+    owner: Owner = Depends(get_authenticated_owner),
+):
+    with session:
+        stmt = select(FootballField).where(FootballField.id == field_id)
+        field: FootballField = session.scalar(stmt)
+
+        if not field:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Field not found",
+            )
+
+        if field.owner_id != owner.id:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not the owner of this field",
+            )
+
+        for key, value in dict(vars(data).items()).items():
+            if value:
+                setattr(field, key, value)
+
+        session.add(field)
+        session.commit()
+
+
+@router.get("/{field_id}", status_code=status.HTTP_200_OK)
+def get_field(field_id: int):
+    with session:
+        stmt = select(FootballField).where(FootballField.id == field_id)
+        return session.scalar(stmt).json()
+
+
+@router.delete(
+    "/{field_id}",
+    status_code=status.HTTP_200_OK,
+)
+def delete_field(
+    field_id: int, owner: Owner = Depends(get_authenticated_owner)
+):
+    with session:
+        stmt = select(FootballField).where(FootballField.id == field_id)
+        field: FootballField = session.scalar(stmt)
+
+        if not field:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Field not found",
+            )
+
+        if field.owner_id != owner.id:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not the owner of this field",
+            )
+
+        session.delete(field)
+        session.commit()
+
+
+@router.get("/", status_code=status.HTTP_200_OK)
+def get_fields():
+    with session:
+        stmt = select(FootballField)
+        return [x.json() for x in session.scalars(stmt)]
